6d063ccdec77f3fc2858a9a51909b2d73c7d9027
Ivan Liauchuk <58084283+ivanliauchuk@users.noreply.github.com>
2022-02-21 18:29:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def sayGenerator():
+    a = 'like'
+
+    for i in range(5):
+        yield i
+        print(a)
+
+
+a = sayGenerator()
+
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print(next(a))
+print()
